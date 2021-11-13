39cc8199eef7f8de1a0d3d00422885d28df4164f
galihpermana29 <galihcbn123@gmail.com>
2021-11-13 21:57:23 +0700

    App.js changes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,28 @@
-import Topbar from "./components/topbar/Topbar";
-import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
-import Settings from "./pages/settings/Settings";
-import Single from "./pages/single/Single";
-import Write from "./pages/write/Write";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Topbar from './components/topbar/Topbar';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Settings from './pages/settings/Settings';
+import Single from './pages/single/Single';
+import Write from './pages/write/Write';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return (
-    <Router>
-      <Topbar />
-      <Routes>
-        <Route exact path="/" element={<Home></Home>}>
-          <Home />
-        </Route>
-        <Route path="/register">
-          <Register />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/write">
-          <Write />
-        </Route>
-        <Route path="/settings">
-          <Settings />
-        </Route>
-        <Route path="/post/:postId">
-          <Single />
-        </Route>
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Topbar />
+			<div>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/register" element={<Register />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/write" element={<Write />} />
+					<Route path="/settings" element={<Settings />} />
+					<Route path="/post/:postId" element={<Single />} />
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
